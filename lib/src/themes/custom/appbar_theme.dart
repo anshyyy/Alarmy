e3f88f957e3aba34334dd28d2dfd403d643b21bf
Anshuman Sharma <anshuman9998@gmail.com>
2024-05-28 17:59:26 +0530

    themes and intial screen

---
+++ lib/src/themes/custom/appbar_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class KAppBarTheme {
+  KAppBarTheme._();
+
+  static AppBarTheme light = AppBarTheme(
+    elevation: 0,
+    centerTitle: false,
+    scrolledUnderElevation: 0,
+    backgroundColor: Colors.white,
+    surfaceTintColor: Colors.transparent,
+    iconTheme: IconThemeData(color: Colors.black, size: 24),
+    actionsIconTheme: IconThemeData(color: Colors.black, size: 24),
+    titleTextStyle: TextStyle(
+        fontSize: 18, fontWeight: FontWeight.w600, color: Colors.black),
+  );
+  static AppBarTheme dark = AppBarTheme(
+    elevation: 0,
+    centerTitle: false,
+    scrolledUnderElevation: 0,
+    backgroundColor: Colors.black,
+    surfaceTintColor: Colors.transparent,
+    iconTheme: IconThemeData(color: Colors.white, size: 24),
+    actionsIconTheme: IconThemeData(color: Colors.white, size: 24),
+    titleTextStyle: TextStyle(
+        fontSize: 18, fontWeight: FontWeight.w600, color: Colors.white),
+  );
+}
---
+++ lib/src/themes/custom/elevatedbutton_theme.dart
@@ -0,0 +1,31 @@
+import "package:flutter/material.dart";
+
+class KElevatedButtonTheme {
+  KElevatedButtonTheme._();
+  static ElevatedButtonThemeData light = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+        elevation: 0,
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black,
+        disabledBackgroundColor: Colors.grey,
+        disabledForegroundColor: Colors.grey,
+        side: BorderSide(color: Colors.grey),
+        padding: const EdgeInsets.symmetric(vertical: 18),
+        textStyle: const TextStyle(
+            fontSize: 16, color: Colors.white, fontWeight: FontWeight.w500),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+  );
+  static ElevatedButtonThemeData dark = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+        elevation: 0,
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+        disabledBackgroundColor: Colors.grey,
+        disabledForegroundColor: Colors.grey,
+        side: BorderSide(color: Colors.grey),
+        padding: const EdgeInsets.symmetric(vertical: 18),
+        textStyle: const TextStyle(
+            fontSize: 16, color: Colors.white, fontWeight: FontWeight.w500),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))),
+  );
+}
